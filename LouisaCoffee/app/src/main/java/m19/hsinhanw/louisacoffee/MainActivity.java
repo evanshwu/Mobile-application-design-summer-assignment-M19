027892ef3100a32266917a0b497f49c9cd5ba7f8
evanshwu <37651155+evanshwu@users.noreply.github.com>
2019-08-13 12:04:48 -0700

    Fix permission not requesting in foreground

--- LouisaCoffee/app/src/main/java/m19/hsinhanw/louisacoffee/MainActivity.java
+++ LouisaCoffee/app/src/main/java/m19/hsinhanw/louisacoffee/MainActivity.java
@@ -1,8 +1,14 @@
 package m19.hsinhanw.louisacoffee;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
@@ -12,6 +18,7 @@ import android.view.animation.AnimationSet;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.SplashScreenTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale((Activity) this, Manifest.permission.CALL_PHONE)){
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                startActivity(intent);
+            }else{
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 10111);
+            }
+        }else{
+            //CALL
+        }
 
         ImageView imgGallery = findViewById(R.id.logoImageView);
         int gallery[] = {R.drawable.image1, R.drawable.image2, R.drawable.image3, R.drawable.image4};
@@ -92,5 +112,23 @@ public class MainActivity extends AppCompatActivity {
                 // TODO Auto-generated method stub
             }
         });
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode) {
+            case 10111: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Uri callIntentUri = Uri.parse("tel:"+getResources().getString(R.string.phone));
+                    Intent callIntent = new Intent(Intent.ACTION_CALL, callIntentUri);
+                    startActivity(callIntent);
+                } else {
+                    Toast t = Toast.makeText(getApplicationContext(), "Error requesting permission", Toast.LENGTH_SHORT);
+                    t.show();
+                }
+                break;
+            }
+        }
     }
 }
